c0a44c738c62b3385d3ab1045a7175b7d4fed4b1
barsab <b.sabato@unirsm.sm>
2025-11-18 17:36:04 +0100

    Add Props

---
+++ React/esercizio_props/src/App.jsx
@@ -0,0 +1,7 @@
+import Welcome from './Welcome'
+
+function App() {
+  return <Welcome name="Dario" age={33} />
+}
+
+export default App;
---
+++ React/esercizio_props/src/Welcome.jsx
@@ -0,0 +1,10 @@
+import Age from './Age'
+
+export default function Welcome({ name, age }) {
+    return (
+        <>
+            <p>Welcome, <strong>{name}</strong>!</p>
+            <Age age={age} />
+        </>
+    )
+}
